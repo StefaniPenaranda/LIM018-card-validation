5a3c64b84cf1db14bea73747a05806376d5c302f
@StefaniPenaranda <pstefanipm@gmail.com>
2022-06-01 15:38:50 -0500

    cambios agregados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import validator from './validator.js';
- // obteniedo valor del input 
-//let numerotarjeta=document.getElementById(inputNumero).value;
-//console.log(validator.isValid('12345'));
+
+// obteniedo valor del input 
+//let traeNumero = document.getElementById(inputNumero).value;
+let numeroTarjeta= ""
+
+let botonComprar = document.querySelector('.btn-comprar');
+
+let registrarTarjeta = () => {
+ numeroTarjeta= document.querySelector('.inputNumero').value;
+
+if (numeroTarjeta === "" ) return alert("Ingrese su número de tarjeta")
+
+}
+
 console.log(validator.isValid('123456789'));
 console.log(validator.maskify('123456789'));
